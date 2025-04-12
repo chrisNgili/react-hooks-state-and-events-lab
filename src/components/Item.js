184dd720e95a6b63d6eb2b45f5217b6fad67014c
chrisngili <ngilichristopher@gmail.com>
2025-04-13 00:19:34 +0300

    tests passed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart]=useState(false)
+  function addToCart(){
+    setCart((add) => !add)
+  }
+  const cartStock = cart ? "in-cart" : ""
+  const buttonChecker = cart ? "remove" : "add"
+  const buttonText = cart ? "Remove From Cart" : "Add to Cart"
   return (
-    <li className="">
+    <li className={cartStock}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonChecker}   onClick= {addToCart}>{buttonText}</button>
     </li>
   );
 }
